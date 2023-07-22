d0c3ee52320d4e1fc1459c4654f5530f9b27d27c
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-07-22 22:22:33 +0300

    craete model (success message) and fix styling

--- loan-app-form/src/LoanForm.js
+++ loan-app-form/src/LoanForm.js
@@ -1,44 +1,57 @@
 import "./LoanForm.css";
+import Model from "./Model";
+import { useState } from "react";
 
 export default function LoadForm() {
+  const [loan_form, set_loan_form] = useState({
+    name: "",
+    phone: "",
+    age: "",
+    ask: false,
+    salary: "",
+  });
+
   return (
-    <div className="form-container">
-      <form>
-        <h1 style={{ marginBottom: "30px" }}>Requesting a Loan</h1>
-        <div class="inputbox">
-          <input required="required" type="text" />
-          <span>Name</span>
-          <i></i>
-        </div>
-        <div class="inputbox">
-          <input required="required" type="text" />
-          <span>Phone Number</span>
-          <i></i>
-        </div>
-        <div class="inputbox">
-          <input required="required" type="text" />
-          <span>Age</span>
-          <i></i>
-        </div>
-        <div class="inputbox">
-          <labal>Are you a employee ?</labal>
-          <br></br>
-          <br></br>
-          <input type="checkbox" />
-        </div>
-        <div class="inputbox">
-          <labal>Salary</labal>
-          <br></br>
-          <br></br>
-          <select>
-          <option hidden>Choose Salary</option>
-            <option>less than 500$</option>
-            <option>between 500$ and 2000$</option>
-            <option>above 2000$</option>
-          </select>
-        </div>
-        <button>Submit</button>
-      </form>
+    <div id="container">
+      <div id="form-container">
+        <form>
+          <h1 style={{ marginBottom: "30px" }}>Requesting a Loan</h1>
+          <div className="inputbox">
+            <input required="required" type="text" />
+            <span>Name</span>
+            <i></i>
+          </div>
+          <div className="inputbox">
+            <input required="required" type="text" />
+            <span>Phone Number</span>
+            <i></i>
+          </div>
+          <div className="inputbox">
+            <input required="required" type="text" />
+            <span>Age</span>
+            <i></i>
+          </div>
+          <div className="inputbox">
+            <label>Are you a employee ?</label>
+            <br></br>
+            <br></br>
+            <input type="checkbox" />
+          </div>
+          <div className="inputbox">
+            <label>Salary</label>
+            <br></br>
+            <br></br>
+            <select>
+              <option hidden>Choose Salary</option>
+              <option>less than 500$</option>
+              <option>between 500$ and 2000$</option>
+              <option>above 2000$</option>
+            </select>
+          </div>
+          <button>Submit</button>
+        </form>
+      </div>
+      {/* <Model /> */}
     </div>
   );
 }
---
+++ loan-app-form/src/Model.js
@@ -0,0 +1,11 @@
+import "./Model.css";
+
+export default function Model() {
+  return (
+    <div id="model">
+      <div id="model-content">
+        <h1>The Form Has Been Submitted Successfully</h1>
+      </div>
+    </div>
+  );
+}
